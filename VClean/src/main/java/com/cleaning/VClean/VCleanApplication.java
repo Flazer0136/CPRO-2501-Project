307cc0e77c0ef52900b03dba3c305015aea47e4c
Flazer <146836746+Flazer0136@users.noreply.github.com>
2025-03-06 04:11:03 -0700

    Initial Files
    Initial files, template from spring initalizr

---
+++ VClean/src/main/java/com/cleaning/VClean/VCleanApplication.java
@@ -0,0 +1,13 @@
+package com.cleaning.VClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VCleanApplication.class, args);
+	}
+
+}
---
+++ VClean/src/test/java/com/cleaning/VClean/VCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleaning.VClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
